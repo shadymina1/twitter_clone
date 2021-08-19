7bfb2ebcab83e7eaf2fdc2211e1f16177972a9ac
Shady <shady.mina.ca@gmail.com>
2021-08-18 22:26:31 -0400

    cleang

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ require('dotenv').config();
 connectDB();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
